e27f304f8826e3dd28c455134c6bdece636557d2
Louis Wickremeratne <louiswickremeratne@yahoo.co.uk>
2021-01-20 16:04:54 +0000

    Stuck need Dan to try

---
+++ Battle_app/spec/features/players_names_spec.rb
@@ -0,0 +1,9 @@
+feature 'players to fill in their names' do
+  scenario 'players can submit their names' do
+    visit('/')
+    find_in :player_1_name, with: 'Jimmy'
+    fill_in :player_2_name, with: 'Dave'
+    click_button 'Submit'
+    expect(page).to have_content 'Jimmy v Dave'
+  end
+end
